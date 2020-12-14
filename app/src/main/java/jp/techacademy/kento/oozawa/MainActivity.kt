14bb6630e3e3869c68b438b953f37e88e138f508
kento-oozawa <kentomilk0514@gmail.com>
2020-12-14 13:42:32 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/kento/oozawa/MainActivity.kt
@@ -0,0 +1,29 @@
+package jp.techacademy.kento.oozawa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        for (i in 1 until 6) {
+            Log.d("kotlintest", "for文の " + i + "回目")
+
+        }
+
+        val num1 = 10 + 5 - 2 * 4 / 2
+        Log.d("kotlintest", "10 + 5 - 2 * 4 / 2 = " + num1)
+
+        val flag1 = true
+        val flag2 = false
+        Log.d("kotlintest", "flag1 && flag2 = " + (flag1 && flag2))
+        Log.d("kotlintest", "flag1 || flag2 = " + (flag1 || flag2))
+
+        val num2 = 10
+        val num3 = 20
+        Log.d("kotlintest", "num2 < num3 = " + (num2 < num3))
+    }
+}
